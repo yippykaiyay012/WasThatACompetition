a93b534c3c946502dd2affce6cd1806aaa77801f
Ryan <ryanmitchell.dev@gmail.com>
2022-05-17 22:17:24 +0100

    update automatically

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,16 @@
-fetch( 'https://bsite.net/ryan012/' )
+
+function GetData() {
+    fetch( 'https://bsite.net/ryan012/' )
     .then( response => response.json() )
     .then( response => {
         document.getElementById("result-text").innerText = response.result;
     } );
+}
 
+
+document.addEventListener("DOMContentLoaded", function() {
+    GetData();
+  });
+
+
+setInterval(GetData, 5000);
